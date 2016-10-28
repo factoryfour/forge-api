fcc2109e3d0f18c7de18f9a00ef238ddc90832e5
Alex Mathews <alexjmathews@gmail.com>
2016-10-28 15:46:29 -0400

    two leg authentication working

--- resources/auth.js
+++ resources/auth.js
@@ -1,15 +1,36 @@
+var request = require('request');
+
 module.exports = function(config) {
-    console.log(config);
-    var authentication = {};
+    var auth = {};
 
+    auth.two_leg = function(scope, callback) {
 
+        if (!scope || !Array.isArray(scope)) {
+            return callback("Provided scope must be an array.")
+        }
 
+        var scope_string = "";
+        scope.forEach(function(scopeElement) {
+            scope_string += scopeElement + " ";
+        })
 
-    authentication.test = function() {
-        console.log("testing in test " + config);
+        var options = {
+            method: 'POST',
+            url: 'https://developer.api.autodesk.com/authentication/v1/authenticate',
+            form: {
+                client_id: config.CLIENT_ID,
+                client_secret: config.CLIENT_SECRET,
+                grant_type:'client_credentials',
+                scope:scope_string
+            }
+        };
 
+        request(options, function(error, response, body) {
+            if (error) return callback(Error(error), null);
+            return callback(null, body)
+        });
     };
 
-    return authentication;
+    return auth;
 
 }
