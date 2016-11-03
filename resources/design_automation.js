4e0ee22f355fc593b9f2959787b8d0cc7e794760
Alex Mathews <alexjmathews@gmail.com>
2016-11-03 10:35:30 -0400

    Yay engine

--- resources/design_automation.js
+++ resources/design_automation.js
@@ -1,9 +1,11 @@
 var request = require('request');
 
-module.exports = function(config) {
+module.exports = function(config, authObj) {
     var da = {};
 
-    da.engines = require('./design_automation/engines.js'); 
+	da.engines = require('./design_automation/engines.js')(config, authObj);
+	da.app_packages = require('./design_automation/app_packages.js')(config, authObj);
+    da.work_items = require('./design_automation/work_items.js')(config, authObj);
 
     return da;
 
