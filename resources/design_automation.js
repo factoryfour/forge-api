a00142a9956d8701b44070d3f8348879658ee953
Alex Mathews <alexjmathews@gmail.com>
2016-12-02 12:03:47 -0500

    new activities module

--- resources/design_automation.js
+++ resources/design_automation.js
@@ -5,7 +5,8 @@ module.exports = function(config, authObj) {
 
 	da.engines = require('./design_automation/engines.js')(config, authObj);
 	da.app_packages = require('./design_automation/app_packages.js')(config, authObj);
-    da.work_items = require('./design_automation/work_items.js')(config, authObj);
+	da.work_items = require('./design_automation/work_items.js')(config, authObj);
+    da.activities = require('./design_automation/activities.js')(config, authObj);
 
     return da;
 
--- resources/design_automation/activities.js
+++ resources/design_automation/activities.js
@@ -2,9 +2,9 @@ const request = require('request');
 const fs = require('fs');
 
 module.exports = function(config, authObj) {
-    var app_packages = {};
+    var activities = {};
 
-    app_packages.getAll = function(callback) {
+    activities.getAll = function(callback) {
         authObj.getToken(function(error, token) {
             if (error) {
                 return callback(error, null);
@@ -12,7 +12,7 @@ module.exports = function(config, authObj) {
 
             var options = {
                 method: 'GET',
-                url: config.BASE_URL + 'AppPackages',
+                url: config.BASE_URL + 'Activities',
                 headers: {
                     authorization: 'Bearer ' + token
                 }
@@ -32,35 +32,7 @@ module.exports = function(config, authObj) {
         });
     };
 
-    app_packages.getUploadUrl = function(callback) {
-        authObj.getToken(function(error, token) {
-            if (error) {
-                return callback(error, null);
-            }
-
-            var options = {
-                method: 'GET',
-                url: config.BASE_URL + 'AppPackages/Operations.GetUploadUrl',
-                headers: {
-                    authorization: 'Bearer ' + token
-                }
-            };
-
-            request(options, function(error, response, body) {
-                if (error) return callback(Error(error), null);
-                try {
-                    var parsed = JSON.parse(body);
-                    return callback(null, parsed.value);
-                } catch (e) {
-                    return callback("Error parsing response.", null)
-                } finally {
-
-                }
-            });
-        });
-    };
-
-    app_packages.create = function(packageConfig, callback) {
+    activities.create = function(packageConfig, callback) {
         authObj.getToken(function(error, token) {
             if (error) {
                 return callback(error, null);
@@ -68,7 +40,7 @@ module.exports = function(config, authObj) {
 
             var options = {
                 method: 'POST',
-                url: config.BASE_URL + 'AppPackages',
+                url: config.BASE_URL + 'Activities',
                 headers: {
                     authorization: 'Bearer ' + token
                 },
@@ -90,30 +62,7 @@ module.exports = function(config, authObj) {
         });
     };
 
-    app_packages.pushBundle = function(filePath, callback) {
-        app_packages.getUploadUrl(function(error, upload_url) {
-            if (error) {
-                return callback(error);
-            }
-            fs.readFile(filePath, function(err, data) {
-                if (err) {
-                    return console.log(err);
-                }
-                request({
-                    method: "PUT",
-                    url: upload_url,
-                    body: data
-                }, function(error, res, body) {
-                    if (error) {
-                        return callback(error);
-                    }
-                    return callback(null, upload_url);
-                })
-            });
-        });
-    }
-
-	app_packages.delete = function(id, callback) {
+	activities.delete = function(id, callback) {
         authObj.getToken(function(error, token) {
             if (error) {
                 return callback(error, null);
@@ -121,7 +70,7 @@ module.exports = function(config, authObj) {
 
             var options = {
                 method: 'DELETE',
-                url: config.BASE_URL + 'AppPackages(\'' + id + '\')',
+                url: config.BASE_URL + 'Activities(\'' + id + '\')',
                 headers: {
                     authorization: 'Bearer ' + token
                 }
@@ -137,6 +86,6 @@ module.exports = function(config, authObj) {
         });
     };
 
-    return app_packages;
+    return activities;
 
 }
